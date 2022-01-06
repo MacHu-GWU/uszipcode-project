15205f78c72c27e1ae3b796c08d835369d81f6fc
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-01-05 20:25:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -112,15 +112,14 @@ if __name__ == "__main__":
     ]
 
     CLASSIFIERS = [
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS",
         "Operating System :: Unix",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
